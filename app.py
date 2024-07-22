9bd9cb025895f0edfc384877576a8c0062753d25
Soufyane El_Asly <157963593+soufyaneelasly@users.noreply.github.com>
2024-07-22 14:05:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -298,5 +298,7 @@ def logout():
 
 
 if __name__ == "__main__":
+    init_db()
+
     port = int(os.getenv('PORT', 5000))  # Default to 5000 if PORT is not set
     app.run(host='0.0.0.0', port=port)
