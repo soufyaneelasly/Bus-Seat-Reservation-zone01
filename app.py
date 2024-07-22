4378d986e475f9d619337dbf2b0e7783cd9f347d
Soufyane El_Asly <157963593+soufyaneelasly@users.noreply.github.com>
2024-07-22 13:47:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3
+import os
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -296,9 +297,6 @@ def logout():
 
 
 
-
-
-
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.getenv('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
